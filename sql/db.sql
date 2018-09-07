29e133df7315898ff7f84087520b8727e2c025c2
Apxat <apxata@gmail.com>
2018-09-07 23:42:37 +0300

    обновил бд

--- sql/db.sql
+++ sql/db.sql
@@ -1,23 +1,28 @@
--- --------------------------------------------------------
--- Хост:                         localhost
--- Версия сервера:               10.3.9-MariaDB-log - mariadb.org binary distribution
--- Операционная система:         Win64
--- HeidiSQL Версия:              9.5.0.5196
--- --------------------------------------------------------
+-- MySQL dump 10.16  Distrib 10.3.9-MariaDB, for Win64 (AMD64)
+--
+-- Host: 127.0.0.1    Database: blog
+-- ------------------------------------------------------
+-- Server version	10.3.9-MariaDB-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `articles`
+--
 
--- Дамп структуры базы данных blog
-CREATE DATABASE IF NOT EXISTS `blog` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
-USE `blog`;
-
--- Дамп структуры для таблица blog.articles
-CREATE TABLE IF NOT EXISTS `articles` (
+DROP TABLE IF EXISTS `articles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `articles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `author_id` int(11) NOT NULL,
   `create_date` timestamp NOT NULL DEFAULT current_timestamp(),
@@ -28,15 +33,26 @@ CREATE TABLE IF NOT EXISTS `articles` (
   `visible` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Дамп данных таблицы blog.articles: ~2 rows (приблизительно)
+--
+-- Dumping data for table `articles`
+--
+
+LOCK TABLES `articles` WRITE;
 /*!40000 ALTER TABLE `articles` DISABLE KEYS */;
-INSERT INTO `articles` (`id`, `author_id`, `create_date`, `last_edit_date`, `preview_text`, `full_text`, `subject`, `visible`) VALUES
-	(1, 20, '2018-08-16 09:31:43', '0000-00-00 00:00:00', '', 'Новая статья \r\n\r\n\r\n**Новая статья **\r\n\r\n*Новая статья *', 'Новая статья ', 1);
+INSERT INTO `articles` VALUES (1,20,'2018-08-16 09:31:43','0000-00-00 00:00:00','','Новая статья \r\n\r\n\r\n**Новая статья **\r\n\r\n*Новая статья *','Новая статья ',1);
 /*!40000 ALTER TABLE `articles` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- Дамп структуры для таблица blog.comments
-CREATE TABLE IF NOT EXISTS `comments` (
+--
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comments` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `article_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
@@ -47,50 +63,84 @@ CREATE TABLE IF NOT EXISTS `comments` (
   KEY `user_id` (`user_id`),
   CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Дамп данных таблицы blog.comments: ~1 rows (приблизительно)
+--
+-- Dumping data for table `comments`
+--
+
+LOCK TABLES `comments` WRITE;
 /*!40000 ALTER TABLE `comments` DISABLE KEYS */;
-INSERT INTO `comments` (`id`, `article_id`, `user_id`, `date_create`, `comment`, `deleted`) VALUES
-	(1, 2, 1, '2018-09-07 07:53:49', 'раз раз', 0);
+INSERT INTO `comments` VALUES (1,2,1,'2018-09-07 07:53:49','раз раз',0);
 /*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- Дамп структуры для таблица blog.fame
-CREATE TABLE IF NOT EXISTS `fame` (
+--
+-- Table structure for table `fame`
+--
+
+DROP TABLE IF EXISTS `fame`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `fame` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `fame` int(11) NOT NULL DEFAULT 0,
   `fame_time` timestamp NOT NULL DEFAULT current_timestamp(),
   `timespent` int(11) DEFAULT NULL,
   `fame_get` int(11) DEFAULT NULL,
   `fame_per_hour` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Дамп данных таблицы blog.fame: ~0 rows (приблизительно)
+--
+-- Dumping data for table `fame`
+--
+
+LOCK TABLES `fame` WRITE;
 /*!40000 ALTER TABLE `fame` DISABLE KEYS */;
-INSERT INTO `fame` (`id`, `fame`, `fame_time`, `timespent`, `fame_get`, `fame_per_hour`) VALUES
-	(1, 1000, '2018-09-07 08:11:06', 1536307866, 1000, 0),
-	(2, 2000, '2018-09-07 08:11:13', 7, 1000, 514286);
+INSERT INTO `fame` VALUES (1,1000,'2018-09-07 08:11:06',1536307866,1000,0,1),(2,2000,'2018-09-07 08:11:13',7,1000,514286,1),(3,5000,'2018-09-07 08:47:29',2176,3000,4963,1),(4,10000,'2018-09-07 08:55:27',478,5000,37657,1),(5,10000,'2018-09-07 08:56:41',NULL,NULL,NULL,1),(6,15000,'2018-09-07 08:56:54',NULL,NULL,NULL,1),(7,20000,'2018-09-07 08:57:39',45,5000,400000,1),(8,150000,'2018-09-07 09:48:20',3041,130000,153897,1),(9,10000,'2018-09-07 19:30:40',34940,-140000,-14425,1),(10,12500,'2018-09-07 19:30:50',10,2500,900000,2),(11,13500,'2018-09-07 19:47:03',983,3500,12818,1),(12,14500,'2018-09-07 19:48:32',89,1000,40449,1);
 /*!40000 ALTER TABLE `fame` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- Дамп структуры для таблица blog.users
-CREATE TABLE IF NOT EXISTS `users` (
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
   `hashed_password` varchar(255) NOT NULL,
   `reg_date` timestamp NOT NULL DEFAULT current_timestamp(),
-  `first_name` varchar(50) DEFAULT NULL,
+  `nickname` varchar(50) NOT NULL,
   `last_name` varchar(50) DEFAULT NULL,
   `deleted` tinyint(4) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Дамп данных таблицы blog.users: ~0 rows (приблизительно)
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` (`id`, `email`, `hashed_password`, `reg_date`, nickname, `last_name`, `deleted`) VALUES
-	(1, 'apxata@gmail.com', '$2y$10$E2arR9d7kS6rH64oCgE5deITM4n3pJ/s1Jus5/OkrGctRyAuD1Nhm', '2018-08-01 09:26:19', '', '', 0);
+INSERT INTO `users` VALUES (1,'apxata@gmail.com','$2y$10$E2arR9d7kS6rH64oCgE5deITM4n3pJ/s1Jus5/OkrGctRyAuD1Nhm','2018-08-01 09:26:19','Apxat','',0);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-09-07 23:42:01
